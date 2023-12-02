a8479c9b2b77d3f381d4975810e4658687ee42b6
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-12-02 15:19:34 -0500

    Update merge_vcf.py

--- Brinimage/mrge_GPCVCF_Disease_sumstat_vcf/merge_vcf.py
+++ Brinimage/mrge_GPCVCF_Disease_sumstat_vcf/merge_vcf.py
@@ -6,17 +6,24 @@ bcftools merge \
 
 tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz
 
+
+
+##Select SNPS only from the vcf file
 bcftools view \
       --types snps dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz | bgzip -c >dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3_SNPOnly.vcf.gz
 
 tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3_SNPOnly.vcf.gz
 
 
+## Remove multi alleleic variants
+bcftools norm --multiallelics +any \
+        dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3_SNPOnly.vcf.gz |bcftools view --max-alleles 2 \
+        bgzip -c > dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3_SNPOnly_NoMAV.vcf.gz
+        
 
 
 
 ####GenomicPCA_Correlation
-
 bcftools merge \
       dbscan_clust_1_15_GenomicPCA_Correlation.N_weighted_GWAMA.results.vcf.gz \
       Disease_Phenotype_GWAS/MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz | bgzip -c > dbscan_clust_1_15_GenomicPCA_Correlation_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz 
@@ -24,7 +31,12 @@ bcftools merge \
 tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz
 
 
+##Select SNPS only from the vcf file
 bcftools view \
       --types snps dbscan_clust_1_15_GenomicPCA_Correlation_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz | bgzip -c >dbscan_clust_1_15_GenomicPCA_Correlation_MDDwoBP_BIP_pgc3_SCZ_PGC3_SNPOnly.vcf.gz
 
 tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_MDDwoBP_BIP_pgc3_SCZ_PGC3_SNPOnly.vcf.gz
+
+
+
+
