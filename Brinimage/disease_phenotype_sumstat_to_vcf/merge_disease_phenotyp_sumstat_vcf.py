b8de291f9a173bbfc2ae3bdfbd13a1e1af308d98
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-12-03 01:06:06 -0500

    Update merge_disease_phenotyp_sumstat_vcf.py

--- Brinimage/disease_phenotype_sumstat_to_vcf/merge_disease_phenotyp_sumstat_vcf.py
+++ Brinimage/disease_phenotype_sumstat_to_vcf/merge_disease_phenotyp_sumstat_vcf.py
@@ -5,6 +5,6 @@ bcftools merge \
        pgc-bip2021-all.vcf.gz daner_bip_pgc3_nm_noukbiobank.vcf.gz \
        daner_MDDwoBP_20201001_2015iR15iex_HRC_MDDwoBP_iPSYCH2015i_Wray_FinnGen_MVPaf_2_HRC_MAF01.vcf.gz \
        Lam_et_al_2021_CognitiveTaskPerformance.vcf.gz \
-       ADHD2022_iPSYCH_deCODE_PGC.meta.vcf.gz | bgzip -c > Disease_Pheenotype_Sumstat.vcf.gz
+       ADHD2022_iPSYCH_deCODE_PGC.meta.vcf.gz EA4_additive_excl_23andMe.vcf.gz | bgzip -c > Disease_Pheenotype_Sumstat.vcf.gz
 
 tabix -p vcf Disease_Pheenotype_Sumstat.vcf.gz
