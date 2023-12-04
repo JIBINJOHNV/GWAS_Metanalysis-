8121b69d2bebd929c5f4390e61e956f3c10fb853
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-12-04 09:43:31 -0500

    Update ldsc_input_file_preeparation.py

--- Brinimage/ldsc_input_file_preeparation.py
+++ Brinimage/ldsc_input_file_preeparation.py
@@ -1,5 +1,5 @@
 
-
+## Correlation
 bcftools merge \
      dbscan_clust_1_15_GenomicPCA_Correlation.N_weighted_GWAMA.results.vcf.gz \
      /edgehpc/dept/human_genetics/users/jjohn1/Outcome_GWAS/All_GPCA_MetaSumstat/Disease_Phenotype_GWAS/Disease_Pheenotype_Sumstat.vcf.gz | \
@@ -7,13 +7,28 @@ bcftools merge \
 
 tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype.vcf.gz
 
+bcftools view --types snps dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype.vcf.gz | \
+       bgzip -c >dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz
+tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz
 
+
+#bcftools norm --multiallelics -any dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz |\
+#|awk -F"\t" '++seen[$1,$2] == 1'
+
+
+## covariates
 bcftools merge \
      dbscan_clust_2_15_GenomicPCA_covariates.N_weighted_GWAMA.results.vcf.gz \
      /edgehpc/dept/human_genetics/users/jjohn1/Outcome_GWAS/All_GPCA_MetaSumstat/Disease_Phenotype_GWAS/Disease_Pheenotype_Sumstat.vcf.gz | \
-     bgzip -c > dbscan_clust_1_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz
+     bgzip -c > dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz
 
-tabix -p vcf dbscan_clust_1_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz
+tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz 
+
+bcftools view --types snps dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz | \
+       bgzip -c >dbscan_clust_2_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz
+
+tabix -p vcf dbscan_clust_2_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz
+
 
 
 
