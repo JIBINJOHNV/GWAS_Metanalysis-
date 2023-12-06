fbb30c1b4619bbab7e2364f90bcbc11896d8c432
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-12-06 07:38:09 -0500

    Update merge_vcf.py

--- Brinimage/mrge_GPCVCF_Disease_sumstat_vcf/merge_vcf.py
+++ Brinimage/mrge_GPCVCF_Disease_sumstat_vcf/merge_vcf.py
@@ -2,9 +2,11 @@
 #### GenomicPCA_covariates
 bcftools merge \
         dbscan_clust_2_15_GenomicPCA_covariates.N_weighted_GWAMA.results.vcf.gz \
-        Disease_Phenotype_GWAS/MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz | bgzip -c > dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz 
+        Disease_Phenotype_GWAS/Disease_Pheenotype_Sumstat.vcf.gz \
+        /mnt/depts/dept04/human_genetics/users/jjohn1/Outcome_GWAS/All_GPCA_MetaSumstat/Orignal_IDP/IDP_Brainimag.vcf.gz | \
+        bgzip -c > dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz 
 
-tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_MDDwoBP_BIP_pgc3_SCZ_PGC3.vcf.gz
+tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz
 
 
 
