cf2f9b136e4137b840260c8cdf49c46bcc72d083
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-12-06 07:34:27 -0500

    Update meerge_vcf.sh

--- Brinimage/BrainImage_IDP_To_vcf/meerge_vcf.sh
+++ Brinimage/BrainImage_IDP_To_vcf/meerge_vcf.sh
@@ -3,3 +3,5 @@ bcftools merge \
             IDP_0356.vcf.gz IDP_0512.vcf.gz \
             IDP_0553.vcf.gz IDP_0628.vcf.gz  \
             IDP_1580.vcf.gz IDP_2089.vcf.gz | bgzip -c > IDP_Brainimag.vcf.gz
+
+tabix -p vcf IDP_Brainimag.vcf.gz
