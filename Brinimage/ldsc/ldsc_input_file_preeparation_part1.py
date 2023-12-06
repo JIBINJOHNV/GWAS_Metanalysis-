bf6d22443f0cc205f631bc62a99b99b04bc35679
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-12-06 07:51:57 -0500

    Update ldsc_input_file_preeparation_part1.py

--- Brinimage/ldsc/ldsc_input_file_preeparation_part1.py
+++ Brinimage/ldsc/ldsc_input_file_preeparation_part1.py
@@ -19,15 +19,16 @@ tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_IDP_SNPO
 ## covariates
 bcftools merge \
      dbscan_clust_2_15_GenomicPCA_covariates.N_weighted_GWAMA.results.vcf.gz \
+     /mnt/depts/dept04/human_genetics/users/jjohn1/Outcome_GWAS/All_GPCA_MetaSumstat/Orignal_IDP/IDP_Brainimag.vcf.gz \
      /edgehpc/dept/human_genetics/users/jjohn1/Outcome_GWAS/All_GPCA_MetaSumstat/Disease_Phenotype_GWAS/Disease_Pheenotype_Sumstat.vcf.gz | \
-     bgzip -c > dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz
+     bgzip -c > dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype_IDP.vcf.gz
 
-tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz 
+tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype_IDP.vcf.gz 
 
-bcftools view --types snps dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype.vcf.gz | \
-       bgzip -c >dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype_SNPOnly.vcf.gz
+bcftools view --types snps dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype_IDP.vcf.gz | \
+       bgzip -c >dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype_IDP_SNPOnly.vcf.gz
 
-tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype_SNPOnly.vcf.gz
+tabix -p vcf dbscan_clust_2_15_GenomicPCA_covariates_Disease_Phenotype_IDP_SNPOnly.vcf.gz
 
 
 
