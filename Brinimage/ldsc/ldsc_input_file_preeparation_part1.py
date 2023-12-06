b3c7bd390dea82b536990aa31a25b58854d1c842
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-12-06 07:40:56 -0500

    Update ldsc_input_file_preeparation_part1.py

--- Brinimage/ldsc/ldsc_input_file_preeparation_part1.py
+++ Brinimage/ldsc/ldsc_input_file_preeparation_part1.py
@@ -1,14 +1,15 @@
 ## Correlation
 bcftools merge \
      dbscan_clust_1_15_GenomicPCA_Correlation.N_weighted_GWAMA.results.vcf.gz \
+     /mnt/depts/dept04/human_genetics/users/jjohn1/Outcome_GWAS/All_GPCA_MetaSumstat/Orignal_IDP/IDP_Brainimag.vcf.gz \
      /edgehpc/dept/human_genetics/users/jjohn1/Outcome_GWAS/All_GPCA_MetaSumstat/Disease_Phenotype_GWAS/Disease_Pheenotype_Sumstat.vcf.gz | \
-     bgzip -c > dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype.vcf.gz
+     bgzip -c > dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_IDP.vcf.gz
 
-tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype.vcf.gz
+tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_IDP.vcf.gz
 
-bcftools view --types snps dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype.vcf.gz | \
-       bgzip -c >dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz
-tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz
+bcftools view --types snps dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_IDP.vcf.gz | \
+       bgzip -c >dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_IDP_SNPOnly.vcf.gz
+tabix -p vcf dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_IDP_SNPOnly.vcf.gz
 
 
 #bcftools norm --multiallelics -any dbscan_clust_1_15_GenomicPCA_Correlation_Disease_Phenotype_SNPOnly.vcf.gz |\
